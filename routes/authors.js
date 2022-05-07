4e0354a0c41a9eb6ebc578462c8edbb17d394f8f
Santiago <santiagov801@gmail.com>
2022-05-07 11:11:05 -0500

    Initial Author rouets

---
+++ routes/authors.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const Author = require('../models/author');
+
+//All Authors Route
+router.get('/', async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = new RegExp(req.query.name, 'i');
+  }
+
+  try {
+    const authors = await Author.find(searchOptions);
+    res.render('authors/index', { authors: authors, searchOptions: req.query });
+  } catch {
+    res.redirect('/');
+  }
+});
+
+//New Authors Route
+router.get('/new', (req, res) => {
+  res.render('authors/new', { author: new Author() });
+});
+
+// Create Author Route
+router.post('/', async (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  });
+
+  try {
+    const newAuthor = await author.save();
+    //res.redirect(`authors/${newAuthor.id}`)
+    console.log(req.body.name);
+    res.redirect('/authors');
+  } catch {
+    console.log('Error');
+
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating author',
+    });
+  }
+});
+
+module.exports = router;
