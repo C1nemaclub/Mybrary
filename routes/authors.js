7e07d88ee6b91b1d0af01d05e80f92f464eb743f
Santiago <santiagov801@gmail.com>
2022-05-09 11:34:12 -0500

    Books/Create/index/new

--- routes/authors.js
+++ routes/authors.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
   }
 
   try {
-    const authors = await Author.find(searchOptions);
+    const authors = await Author.find();
     res.render('authors/index', { authors: authors, searchOptions: req.query });
   } catch {
     res.redirect('/');
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -24,8 +25,8 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
-
 app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Listening on port 3000...');
